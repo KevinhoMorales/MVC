af3403d07020bb274153aaff838a5ade8e0d8d43
Kevin Morales <kfmorales94@gmail.com>
2020-07-30 10:42:39 -0500

    Minor fixes

--- MVC/Coordinator.swift
+++ MVC/Coordinator.swift
@@ -10,7 +10,8 @@ import Foundation
 import UIKit
 
 protocol Coordinator {
-    var navigationController: UINavigationController { get set}
     
+    var navigationController: UINavigationController { get set}
     func start()
+    
 }
